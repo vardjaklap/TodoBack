6146474a9eab66fd15773c1ea9ecbb57b5a46a54
AlexPeresta <alexperesta@gmail.com>
2023-06-05 17:44:23 -0400

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
-export const DB_HOST = 'localhost';
-export const DB_PORT = 5432;
-export const DB_USER = 'postgres';
-export const DB_PASSWORD = 'admin';
-export const DB_NAME = 'todoperch';
+export const DB_HOST = 'DB_HOST';
+export const DB_PORT = 1;
+export const DB_USER = 'DB_USER';
+export const DB_PASSWORD = 'DB_PASSWORD';
+export const DB_NAME = 'DB_NAME';
